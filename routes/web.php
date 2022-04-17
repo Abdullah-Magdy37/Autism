4e1158b0509e8d97ca011d701bb66e2487326707
abdall <abdallamagdytaha615@gmail.com>
2022-04-17 13:26:10 +0200

    new

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,23 @@
 <?php
-
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
     Route::get('/index', 'autism@index' );
-    
+
     Route::get('/enteractive', 'autism@enteractive' )->middleware('auth');
-    
+
     Route::get('/routen', 'autism@routen' )->middleware('auth');
-        
+
     Route::get('/comuni', 'autism@comuni' )->middleware('auth');
-    
+
     Route::get('/', 'autism@index' );
-    
+
     Route::get('/autism', 'autism@autism' );
-    
+
     Route::get('/complex', 'autism@complex' )->middleware('auth');
-    
+
     Route::get('/about', 'autism@about' );
-    
+
     route::post('/interaction/result' , 'resultcontroller@interaction');
 
     route::post('/behaviors/result' , 'resultcontroller@behaviors');
@@ -24,7 +25,7 @@
     route::post('/communication/result' , 'resultcontroller@communication');
 
     route::post('/growth/result' , 'resultcontroller@growth');
-    
+
     Route::get('/elag', 'resultcontroller@index' );
 
 
