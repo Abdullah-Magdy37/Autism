61865bf78ad5ae0c0ce8dec88938e01b4f9694dd
abdall <abdallamagdytaha615@gmail.com>
2022-07-02 22:19:01 +0200

    new

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                     Welcome In Autism Test 
+                     Welcome In Autism Test
                 </div>
 
                 <div class="links">
@@ -89,7 +89,7 @@
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
+                    <a href="https://nova.laravel.com">Novaa</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
